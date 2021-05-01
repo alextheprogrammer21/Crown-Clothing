a37cf98667fec61b95c3f126441bff42c50d8b91
Alex <alextheprogrammer21@gmail.com>
2021-05-01 12:51:07 -0700

    Add react router to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+
+const LiverPage = () => (
+<div>
+  <h1>LIVER PAGE</h1>
+</div>
+);
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route path='/liver' component={LiverPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,29 +12,34 @@ class Directory extends React.Component {
    sections:[{
     title: 'liver',
     imageUrl: 'https://www.tcmworld.org/wp-content/uploads/2014/02/Shutterstock_9915283-Bamboo-Grove.jpg',
-    id: 1
+    id: 1,
+    linkUrl: 'liver'
    },
    {
     title: 'lung',
     imageUrl: 'https://wp.insighttimer.com/blog/wp-content/uploads/2019/11/teapot.jpg',
-    id: 2
+    id: 2,
+    linkUrl: ''
    },
    {
     title: 'heart',
     imageUrl: 'https://shawellnessclinic.com/wp-content/uploads/2018/07/POST-09-2018-.jpg',
-    id: 3
+    id: 3,
+    linkUrl: ''
    },
    {
     title: 'spleen',
     imageUrl: 'https://stickoutyourtongue.files.wordpress.com/2015/05/earth-image-e1432824732147.jpg',
     size:'large',
-    id: 4
+    id: 4,
+    linkUrl: ''
    },
    {
     title: 'kidney',
     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7PX1hqHT7tRMZN6p2baRDDRGRPlNQM1_yoA&usqp=CAU',
     size: 'large',
-    id: 5
+    id: 5,
+    linkUrl: ''
    }]
   }
  }
@@ -42,8 +47,8 @@ class Directory extends React.Component {
  render() {
   return (
   <div className='directory-menu'>
-   {this.state.sections.map(({title, imageUrl, id, size}) => (
-    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+   {this.state.sections.map(({id, ...otherSectionProps }) => (
+    <MenuItem key={id} {...otherSectionProps}/>
    )) }
   </div>
   )
