be53480405d899116ffb1641ba16593da1cecec5
Alex <alextheprogrammer21@gmail.com>
2021-05-02 12:21:14 -0700

    Add items to shop data

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -26,7 +26,49 @@ price: 15
   name: "Jujube seed",
   imageUrl: "",
   price: 15
-  }]}
+  }]},
+  {id: 3,
+    title: 'Kidney',
+   routeName: 'kidney',
+   items: [{
+   id:1,
+   name: "Rhizoma Alismatis",
+   imageUrl: "",
+   price: 10
+   }, {
+    id:2,
+    name: "Tree Peony Bark",
+    imageUrl: "",
+    price: 15
+    }]},
+    {id: 4,
+      title: 'Spleen',
+     routeName: 'spleen',
+     items: [{
+     id:1,
+     name: "Bitter orange immature fruit",
+     imageUrl: "",
+     price: 10
+     }, {
+      id:2,
+      name: "Ginger rhizome fresh",
+      imageUrl: "",
+      price: 15
+      }]},
+      {id: 4,
+        title: 'Lung',
+       routeName: 'lung',
+       items: [{
+       id:1,
+       name: "Brown's lily bulb",
+       imageUrl: "",
+       price: 10
+       }, {
+        id:2,
+        name: "Dong quai root",
+        imageUrl: "",
+        price: 15
+        }]}
 ]
 
 export default SHOP_DATA;
