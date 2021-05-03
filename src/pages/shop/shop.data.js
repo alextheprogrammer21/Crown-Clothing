99e42a297dda576fce91c4e3f1706618ea977d59
Alex <alextheprogrammer21@gmail.com>
2021-05-03 11:27:44 -0700

    modify shop data

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -4,13 +4,13 @@ const SHOP_DATA = [
 routeName: 'liver',
 items: [{
 id:1,
-name: "andrographen",
-imageUrl: "",
+name: "Mentha",
+imageUrl: "https://image.tcmwiki.com/image/bo-he/bo-he2.jpg",
 price: 15
 }, {
  id:2,
  name: "Chrysanthemum",
- imageUrl: "",
+ imageUrl: "https://www.meandqi.com/media/1270/%E8%8F%8A%E8%8A%B1-tcm-shutterstock_1102707263.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
  price: 20
  }]},
  {id: 2,
@@ -19,12 +19,12 @@ price: 15
  items: [{
  id:1,
  name: "Chinese salvia root",
- imageUrl: "",
+ imageUrl: "https://www.dragonherbs.com//media/wysiwyg/major-tonic-herbs/salvia-roots-white-background.png",
  price: 10
  }, {
   id:2,
   name: "Jujube seed",
-  imageUrl: "",
+  imageUrl: "https://www.meandqi.com/media/1316/%E9%85%B8%E6%9E%A3%E4%BB%81-tcm-shutterstock_776311816.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
   price: 15
   }]},
   {id: 3,
@@ -33,12 +33,12 @@ price: 15
    items: [{
    id:1,
    name: "Rhizoma Alismatis",
-   imageUrl: "",
+   imageUrl: "https://image.tcmwiki.com/image/ze-xie/ze-xie3.jpg",
    price: 10
    }, {
     id:2,
     name: "Tree Peony Bark",
-    imageUrl: "",
+    imageUrl: "https://www.herbalshop.com/wp-content/uploads/2015/01/tree_peony_root_bark.jpg",
     price: 15
     }]},
     {id: 4,
@@ -47,12 +47,12 @@ price: 15
      items: [{
      id:1,
      name: "Bitter orange immature fruit",
-     imageUrl: "",
+     imageUrl: "https://image.tcmwiki.com/image/zhi-shi/zhi-shi2.jpg",
      price: 10
      }, {
       id:2,
-      name: "Ginger rhizome fresh",
-      imageUrl: "",
+      name: "Ginger",
+      imageUrl: "https://www.homeperch.com/wp-content/uploads/2018/12/Fresh-ginger-root-dec12.jpg",
       price: 15
       }]},
       {id: 4,
@@ -60,13 +60,13 @@ price: 15
        routeName: 'lung',
        items: [{
        id:1,
-       name: "Brown's lily bulb",
-       imageUrl: "",
+       name: "White mulberry root bark",
+       imageUrl: "https://www.euyansang.com/on/demandware.static/-/Sites-EYSI_International-Library/default/dwcf70bee6/images/herb/294x350-eysi-herbology-WhiteMulberryRoot-Bark.jpg",
        price: 10
        }, {
         id:2,
         name: "Dong quai root",
-        imageUrl: "",
+        imageUrl: "https://cdn.shopify.com/s/files/1/0058/0252/4783/articles/dong-quai-angelica-sinensis-primary_0c5642f1-2d48-45de-9871-9d37c98c1ae9.jpg?v=1556307077",
         price: 15
         }]}
 ]
