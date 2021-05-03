c3f515f07c002b2a2f5667b5ac869a57852ba0b1
Alex <alextheprogrammer21@gmail.com>
2021-05-03 11:56:48 -0700

    add shop images:

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -12,7 +12,20 @@ price: 15
  name: "Chrysanthemum",
  imageUrl: "https://www.meandqi.com/media/1270/%E8%8F%8A%E8%8A%B1-tcm-shutterstock_1102707263.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
  price: 20
- }]},
+ },
+ {
+  id:3,
+  name: "Jujube seed",
+  imageUrl: "https://www.meandqi.com/media/1316/%E9%85%B8%E6%9E%A3%E4%BB%81-tcm-shutterstock_776311816.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
+  price: 15
+  },
+  {
+    id:4,
+    name: "Bitter orange immature fruit",
+     imageUrl: "https://image.tcmwiki.com/image/zhi-shi/zhi-shi2.jpg",
+     price: 10
+    }
+  ]},
  {id: 2,
   title: 'Heart',
  routeName: 'heart',
@@ -26,7 +39,19 @@ price: 15
   name: "Jujube seed",
   imageUrl: "https://www.meandqi.com/media/1316/%E9%85%B8%E6%9E%A3%E4%BB%81-tcm-shutterstock_776311816.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
   price: 15
-  }]},
+  },
+  {
+    id:3,
+    name: "Dong quai root",
+    imageUrl: "https://cdn.shopify.com/s/files/1/0058/0252/4783/articles/dong-quai-angelica-sinensis-primary_0c5642f1-2d48-45de-9871-9d37c98c1ae9.jpg?v=1556307077",
+    price: 15    },
+    {
+      id:4,
+      name: "Chrysanthemum",
+      imageUrl: "https://www.meandqi.com/media/1270/%E8%8F%8A%E8%8A%B1-tcm-shutterstock_1102707263.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
+           price: 20
+      }
+]},
   {id: 3,
     title: 'Kidney',
    routeName: 'kidney',
@@ -40,7 +65,20 @@ price: 15
     name: "Tree Peony Bark",
     imageUrl: "https://www.herbalshop.com/wp-content/uploads/2015/01/tree_peony_root_bark.jpg",
     price: 15
-    }]},
+    },
+    {
+      id:3,
+      name: "Chrysanthemum",
+      imageUrl: "https://www.meandqi.com/media/1270/%E8%8F%8A%E8%8A%B1-tcm-shutterstock_1102707263.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
+      price: 20
+           },
+      {
+        id:4,
+        name: "Chinese salvia root",
+        imageUrl: "https://www.dragonherbs.com//media/wysiwyg/major-tonic-herbs/salvia-roots-white-background.png",
+        price: 10
+               }
+  ]},
     {id: 4,
       title: 'Spleen',
      routeName: 'spleen',
@@ -54,7 +92,20 @@ price: 15
       name: "Ginger",
       imageUrl: "https://www.homeperch.com/wp-content/uploads/2018/12/Fresh-ginger-root-dec12.jpg",
       price: 15
-      }]},
+      },
+      {
+        id:3,
+        name: "Chrysanthemum",
+        imageUrl: "https://www.meandqi.com/media/1270/%E8%8F%8A%E8%8A%B1-tcm-shutterstock_1102707263.jpg?mode=crop&width=600&height=450&format=webp&quality=80",
+        price: 20
+               },
+        {
+          id:4,
+          name: "Dong quai root",
+          imageUrl: "https://cdn.shopify.com/s/files/1/0058/0252/4783/articles/dong-quai-angelica-sinensis-primary_0c5642f1-2d48-45de-9871-9d37c98c1ae9.jpg?v=1556307077",
+          price: 15
+            }
+        ]},
       {id: 4,
         title: 'Lung',
        routeName: 'lung',
@@ -68,7 +119,19 @@ price: 15
         name: "Dong quai root",
         imageUrl: "https://cdn.shopify.com/s/files/1/0058/0252/4783/articles/dong-quai-angelica-sinensis-primary_0c5642f1-2d48-45de-9871-9d37c98c1ae9.jpg?v=1556307077",
         price: 15
-        }]}
+        },
+        {
+          id:3,
+          name: "Mentha",
+          imageUrl: "https://image.tcmwiki.com/image/bo-he/bo-he2.jpg",
+          price: 15
+                    },
+          {
+            id:4,
+            name: "Bitter orange immature fruit",
+            imageUrl: "https://image.tcmwiki.com/image/zhi-shi/zhi-shi2.jpg",
+            price: 10
+                   }]}
 ]
 
 export default SHOP_DATA;
