77344b68d73535f11a93aa8bf82b9d10fd576329
Alex <alextheprogrammer21@gmail.com>
2021-05-04 10:44:08 -0700

    style google sign in

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSIgnUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-function App() {
+import { auth } from './firebase/firebase.utils.js'
+import { render } from '@testing-library/react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null; 
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth(); 
+  }
+
+render() {
   return (
     <div className="App">
-      <Header />
+      <Header currentUser={this.state.currentUser}/>
       <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route path='/shop' component={ShopPage}/>
@@ -17,6 +41,7 @@ function App() {
       </Switch>
     </div>
   );
+} 
 }
 
 export default App;
