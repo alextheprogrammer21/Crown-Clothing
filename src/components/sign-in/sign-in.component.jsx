b60dc8ae2ff7329a9852becee635e6eb795480f9
Alex <alextheprogrammer21@gmail.com>
2021-05-06 12:36:15 -0700

    Complete sign in and sign up functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -22,7 +22,7 @@ class SignIn extends React.Component {
   const {email, password } = this.state;
 
   try {
-       await auth.SignInWithEmailAndPassword(email, password)
+       await auth.signInWithEmailAndPassword(email, password)
        this.setState({email: '', password: ''})
   } catch (err) {
        console.error(err)
