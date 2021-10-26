04bdb5a6815f9bfea3159224684ac3f23f1d5ce8
Alex Setia <56317276+alextheprogrammer21@users.noreply.github.com>
2021-10-26 09:22:27 -0700

    Update stripe-button.component.jsx

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -11,7 +11,7 @@ const StripeCheckoutButton = ({ price }) => {
  return (
   <StripeCheckout 
   label= 'Pay Now'
-  name = 'Shinrinyoku Herb Ltd.'
+  name = 'Crown Clothing Ltd.'
   billingAddress
   shippingAddress
   description={`Your total is $${price}`}
