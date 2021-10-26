75f791194dd71fc28d84ed1e4b7b4ce374f17ad8
Alex Setia <56317276+alextheprogrammer21@users.noreply.github.com>
2021-10-26 09:19:54 -0700

    Update directory.reducer.js

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,43 +1,45 @@
 const INITIAL_STATE = {
- sections:[{
-  title: 'liver',
-  imageUrl: 'https://www.tcmworld.org/wp-content/uploads/2014/02/Shutterstock_9915283-Bamboo-Grove.jpg',
-  id: 1,
-  linkUrl: 'shop/liver'
- },
- {
-  title: 'lung',
-  imageUrl: 'https://wp.insighttimer.com/blog/wp-content/uploads/2019/11/teapot.jpg',
-  id: 2,
-  linkUrl: 'shop/lungs'
- },
- {
-  title: 'heart',
-  imageUrl: 'https://shawellnessclinic.com/wp-content/uploads/2018/07/POST-09-2018-.jpg',
-  id: 3,
-  linkUrl: 'shop/heart'
- },
- {
-  title: 'spleen',
-  imageUrl: 'https://stickoutyourtongue.files.wordpress.com/2015/05/earth-image-e1432824732147.jpg',
-  size:'large',
-  id: 4,
-  linkUrl: 'shop/spleen'
- },
- {
-  title: 'kidney',
-  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7PX1hqHT7tRMZN6p2baRDDRGRPlNQM1_yoA&usqp=CAU',
-  size: 'large',
-  id: 5,
-  linkUrl: 'shop/kidney'
- }]
-}
+  sections: [
+    {
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      linkUrl: 'shop/hats'
+    },
+    {
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      id: 2,
+      linkUrl: 'shop/jackets'
+    },
+    {
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      id: 3,
+      linkUrl: 'shop/sneakers'
+    },
+    {
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens'
+    },
+    {
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    }
+  ]
+};
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
- switch(action.type) {
-  default:
-   return state;
- }
-}
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export default directoryReducer;
